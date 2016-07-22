5160a24fb37f4b141cb92e96e1db9dd784afe5bc
Daniel Wolf <danielrwolf5@gmail.com>
2016-07-21 22:25:41 -0500

    adding templates

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-	return HttpResponse("hello, man, hello.")
+    return render(request, 'posts/post.html')
